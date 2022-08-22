c661a53aa26ed79fe127990c3ad9b5d9b240d17c
YoniBinder <binderyoni@gmail.com>
2022-08-22 22:53:32 +0300

    add more basic functions

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,25 +8,24 @@ contract Token {
     string public name = "My Token";
     string public symbol = "YB";
 
-    uint256 public totalSupply = 1000000;
-
-    address public owner = msg.sender; // just for testing
+    uint256 public _totalSupply = 1000000;
 
     mapping(address => uint256) public balances;
+    mapping(address => mapping (address => uint256)) allowed;
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor() {
-        balances[msg.sender] = totalSupply;
+        balances[msg.sender] = _totalSupply;
     }
 
     /**
      * minting function
      */
     function mint(uint256 amount) external {
-        require(msg.sender == owner, "Only the owner can burn coins");
         balances[msg.sender] += amount;
-        totalSupply += amount;
+        _totalSupply += amount;
         emit Transfer(address(0), msg.sender, amount);
     }
 
@@ -34,14 +33,13 @@ contract Token {
      * burning function
      */
     function burn(uint256 amount) external {
-        require(msg.sender == owner, "Only the owner can burn coins");
         require(
             balances[msg.sender] >= amount,
             "The amount can't exceed the owner's balance"
         );
 
         balances[msg.sender] -= amount;
-        totalSupply -= amount;
+        _totalSupply -= amount;
         emit Transfer(msg.sender, address(0), amount);
     }
 
@@ -50,5 +48,54 @@ contract Token {
     //  */
     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
+    }
+
+
+    // /**
+    //  * transfer from msg.sender to recipient
+    //  */
+  function transfer(address recipient, uint amount) external returns (bool) {
+        require(balances[msg.sender]>=amount, "The amount exceeds available tokens");
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    // /**
+    //  * approve the amount to spend
+    //  */
+    function approve(address spender, uint amount) external returns (bool) {
+        allowed[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    // /**
+    //  * return the total supply
+    //  */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function transferFrom(
+        address owner,
+        address buyer,
+        uint amount
+    ) external returns (bool) {
+        require(amount <= balances[owner]);
+        require(amount <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner]-amount;
+        allowed[owner][msg.sender] -= amount;
+        balances[buyer] += amount;
+        emit Transfer(owner, buyer, amount);
+        return true;
+    }
+    
+    
+
+ function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
     }
 }
